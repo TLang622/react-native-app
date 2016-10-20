d6e9215b3a1c814a0369e1ad9324de33a38d0fcf
lang <1822731716@qq.com>
2016-10-20 11:20:55 +0800

    update

--- app/ui/Index.js
+++ app/ui/Index.js
@@ -9,19 +9,30 @@ import {
     Text,
     AsyncStorage,
     ListView,
-    StyleSheet
+    StyleSheet,
+    Image,
+    ScrollView
 } from 'react-native';
 import Main from '../ui/Main';
 import Tab1 from '../ui/Tab1';
 import Tab2 from '../ui/Tab2';
-import practice from '../practice';
+import Tab3 from '../ui/Tab3';
+import SettingView1 from '../ui/SettingView1';
+import SettingView2 from '../ui/SettingView2';
+import SettingView3 from '../ui/SettingView3';
+import SettingView4 from '../ui/SettingView4';
+import SettingView5 from '../ui/SettingView5';
+import SettingView6 from '../ui/SettingView6';
+import practiceData from '../practice';
+import settingData from '../setting';
 
 export default class Index extends Component {
     constructor(props){
         super(props);
         this.state = {
         dataSource: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2}),
-        loaded: false
+        loaded: false,
+        settingSource: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2}).cloneWithRows(settingData)
       };
     }
 
@@ -53,7 +64,7 @@ export default class Index extends Component {
         });
 
         this.setState({
-          dataSource: this.state.dataSource.cloneWithRows(practice),
+          dataSource: this.state.dataSource.cloneWithRows(practiceData),
           loaded: true,
         });
     }
@@ -64,6 +75,16 @@ export default class Index extends Component {
         navigator.push({
           name : 'Tab2',
           component : Tab2,
+        });
+      }
+    }
+
+    onFormalTest() {
+      const { navigator } = this.props;
+      if (navigator) {
+        navigator.push({
+          name : 'Tab3',
+          component : Tab3,
         });
       }
     }
@@ -86,14 +107,62 @@ export default class Index extends Component {
       }
     }
 
+    onSetting(id) {
+      var myName, myComponent;
+      switch(id) {
+        case '1':
+          myName = 'SettingView1';
+          myComponent = SettingView1;
+          break;
+        case '2':
+          myName = 'SettingView2';
+          myComponent = SettingView2;
+          break;
+        case '3':
+          myName = 'SettingView3';
+          myComponent = SettingView3;
+          break;
+        case '4':
+          myName = 'SettingView4';
+          myComponent = SettingView4;
+          break;
+        case '5':
+          myName = 'SettingView5';
+          myComponent = SettingView5;
+          break;
+        case '6':
+          myName = 'SettingView6';
+          myComponent = SettingView6;
+          break;
+      }
+      const { navigator } = this.props;
+      if (navigator) {
+        navigator.push({
+          name : myName,
+          component : myComponent,
+        });
+      }
+    }
+
     renderRow(rowData) {
       return (
         <TouchableOpacity onPress={this.onPractice.bind(this)}>
           <View style={ListStyle.listRow}>
-            <Text>{rowData.icon}</Text>
+            <Image source={require('../image/icon.png')} />
             <Text>{rowData.name}</Text>
-            <Text>{rowData.star}</Text>
-            <Text>{rowData.practice}</Text>
+            <Image source={require('../image/icon.png')} />
+            <Image source={require('../image/icon.png')} />
+          </View>
+        </TouchableOpacity>
+        )
+    }
+
+    settingRow(rowData) {
+      return (
+        <TouchableOpacity onPress={() => {this.onSetting(rowData.id)}}>
+          <View style={ListStyle.listRow}>
+            <Image source={require('../image/icon.png')} />
+            <Text>{rowData.text}</Text>
           </View>
         </TouchableOpacity>
         )
@@ -105,19 +174,27 @@ export default class Index extends Component {
         }
         return (
             <ScrollableTabView tabBarPosition='bottom' renderTabBar={() => <DefaultTabBar />}>
-              <View tabLabel='刷题'>
-                <ListView
-                  dataSource={this.state.dataSource}
-                  renderRow={this.renderRow.bind(this)}
-                />
-              </View>
+              <ListView
+                tabLabel='刷题'
+                dataSource={this.state.dataSource}
+                renderRow={this.renderRow.bind(this)}
+              />
               <View tabLabel='模拟考试'>
                 <TouchableOpacity onPress={this.onTest.bind(this)}>
-                  <Text>点击考试</Text>
+                  <Text>点击开考</Text>
                 </TouchableOpacity>
               </View>
-              <View tabLabel='正式考试'><Text>fuck</Text></View>
-              <View tabLabel='我的'><Text>fuck</Text></View>
+              <ScrollView tabLabel='正式考试'>
+                <Text>深秋的夜晚，多的是一些凄冷，也曾于薄凉的夜风中，感受那段炙热的情感，还有在岁月中走过留下的旖旎风光。也曾把无数的相思寄语夜空的明月，回首间，少去一份心酸，多一份幸福与安然便好。也曾将深深的牵挂，寄语划过夜幕的流星，只要能在有你的地点漂落，哪怕一瞬也好，可这美好的愿景，终经不起光阴的考验。心冷了，一切也就跟着寂灭了。心字成灰，情随缘灭，一生颠沛流离，只是为了幸福，寻找心灵可以栖息的港湾。梦落潇湘，只是为了幸福，寻找生命最后的归宿。也只愿花落时，不会再是曲终人散的结局。也只愿易逝的流光里，不会再有曲终人散的伤感。相遇有梦，只是这曲曲折折的故事中，幸福的导演着开始，一路走来，却发现彼此心与心的距离越来越远，于是，浪漫的童话里，你成了主角，我却变成了配角。关于那段相遇的记忆，或许现在的你已将与我有关的故事，剔除的干干净净。思念就像歌里的一样，是一种很悬的东西，让人琢磨不定，对朋友的思念只是希望看到他过的好不好，想跟他说说自己的心里话，想跟他玩一玩、聚一聚。爱意的思念是很奇妙的，想要知道他的一切，有时候会想到他有没有在安静的时候想起你来，想拥抱他，给他幸福，想与他以前相处的一点一滴，情不自禁的脸上有笑有泪，这是思念一个最爱的人的最美妙的时候，不论你做什么事情都会想到他，尤其是做和他在一起做过的事情，你可能就会停在那里慢慢被回忆占据。也会在清晨醒刚睡醒的时候就莫名的失神，因为你还没有从梦中醒过来，还没有习惯没有他在你身边的生活。而亲人间的思念又是另一番模样，那是带着浓浓的暖意的哀愁。思念就像歌里的一样，是一种很悬的东西，让人琢磨不定，对朋友的思念只是希望看到他过的好不好，想跟他说说自己的心里话，想跟他玩一玩、聚一聚。爱意的思念是很奇妙的，想要知道他的一切，有时候会想到他有没有在安静的时候想起你来，想拥抱他，给他幸福，想与他以前相处的一点一滴，情不自禁的脸上有笑有泪，这是思念一个最爱的人的最美妙的时候，不论你做什么事情都会想到他，尤其是做和他在一起做过的事情，你可能就会停在那里慢慢被回忆占据。也会在清晨醒刚睡醒的时候就莫名的失神，因为你还没有从梦中醒过来，还没有习惯没有他在你身边的生活。而亲人间的思念又是另一番模样，那是带着浓浓的暖意的哀愁。思念就像歌里的一样，是一种很悬的东西，让人琢磨不定，对朋友的思念只是希望看到他过的好不好，想跟他说说自己的心里话，想跟他玩一玩、聚一聚。爱意的思念是很奇妙的，想要知道他的一切，有时候会想到他有没有在安静的时候想起你来，想拥抱他，给他幸福，想与他以前相处的一点一滴，情不自禁的脸上有笑有泪，这是思念一个最爱的人的最美妙的时候，不论你做什么事情都会想到他，尤其是做和他在一起做过的事情，你可能就会停在那里慢慢被回忆占据。也会在清晨醒刚睡醒的时候就莫名的失神，因为你还没有从梦中醒过来，还没有习惯没有他在你身边的生活。而亲人间的思念又是另一番模样，那是带着浓浓的暖意的哀愁。思念就像歌里的一样，是一种很悬的东西，让人琢磨不定，对朋友的思念只是希望看到他过的好不好，想跟他说说自己的心里话，想跟他玩一玩、聚一聚。爱意的思念是很奇妙的，想要知道他的一切，有时候会想到他有没有在安静的时候想起你来，想拥抱他，给他幸福，想与他以前相处的一点一滴，情不自禁的脸上有笑有泪，这是思念一个最爱的人的最美妙的时候，不论你做什么事情都会想到他，尤其是做和他在一起做过的事情，你可能就会停在那里慢慢被回忆占据。也会在清晨醒刚睡醒的时候就莫名的失神，因为你还没有从梦中醒过来，还没有习惯没有他在你身边的生活。而亲人间的思念又是另一番模样，那是带着浓浓的暖意的哀愁。思念就像歌里的一样，是一种很悬的东西，让人琢磨不定，对朋友的思念只是希望看到他过的好不好，想跟他说说自己的心里话，想跟他玩一玩、聚一聚。爱意的思念是很奇妙的，想要知道他的一切，有时候会想到他有没有在安静的时候想起你来，想拥抱他，给他幸福，想与他以前相处的一点一滴，情不自禁的脸上有笑有泪，这是思念一个最爱的人的最美妙的时候，不论你做什么事情都会想到他，尤其是做和他在一起做过的事情，你可能就会停在那里慢慢被回忆占据。也会在清晨醒刚睡醒的时候就莫名的失神，因为你还没有从梦中醒过来，还没有习惯没有他在你身边的生活。而亲人间的思念又是另一番模样，那是带着浓浓的暖意的哀愁。思念就像歌里的一样，是一种很悬的东西，让人琢磨不定，对朋友的思念只是希望看到他过的好不好，想跟他说说自己的心里话，想跟他玩一玩、聚一聚。爱意的思念是很奇妙的，想要知道他的一切，有时候会想到他有没有在安静的时候想起你来，想拥抱他，给他幸福，想与他以前相处的一点一滴，情不自禁的脸上有笑有泪，这是思念一个最爱的人的最美妙的时候，不论你做什么事情都会想到他，尤其是做和他在一起做过的事情，你可能就会停在那里慢慢被回忆占据。也会在清晨醒刚睡醒的时候就莫名的失神，因为你还没有从梦中醒过来，还没有习惯没有他在你身边的生活。而亲人间的思念又是另一番模样，那是带着浓浓的暖意的哀愁。思念就像歌里的一样，是一种很悬的东西，让人琢磨不定，对朋友的思念只是希望看到他过的好不好，想跟他说说自己的心里话，想跟他玩一玩、聚一聚。爱意的思念是很奇妙的，想要知道他的一切，有时候会想到他有没有在安静的时候想起你来，想拥抱他，给他幸福，想与他以前相处的一点一滴，情不自禁的脸上有笑有泪，这是思念一个最爱的人的最美妙的时候，不论你做什么事情都会想到他，尤其是做和他在一起做过的事情，你可能就会停在那里慢慢被回忆占据。也会在清晨醒刚睡醒的时候就莫名的失神，因为你还没有从梦中醒过来，还没有习惯没有他在你身边的生活。而亲人间的思念又是另一番模样，那是带着浓浓的暖意的哀愁。</Text>
+                <TouchableOpacity onPress={this.onFormalTest.bind(this)}>
+                  <Text>同意</Text>
+                </TouchableOpacity>
+              </ScrollView>
+              <ListView
+                tabLabel='我的'
+                dataSource={this.state.settingSource}
+                renderRow={this.settingRow.bind(this)}
+              />
             </ScrollableTabView>
         );
     }
---
+++ app/ui/SettingView6.js
@@ -0,0 +1,78 @@
+import React, { Component, PropTypes } from 'react';
+import Storage from 'react-native-storage';
+import { 
+	Navigator,
+	Text,
+	TouchableHighlight,
+	View,
+	AsyncStorage 
+} from 'react-native';
+import Toolbar from '../lib/Toolbar';
+import Index from '../ui/Index';
+
+export default class Tab6 extends Component {
+	constructor(props){
+        super(props);
+        this.state = {};
+    }
+	onIcon(){
+		const { navigator } = this.props;
+    if (navigator) {
+      navigator.push({
+        name : 'Index',
+        component : Index,
+      });
+    }
+	}
+  render() {
+    return (
+      <Navigator
+        initialRoute={{ title: 'My Initial Scene', index: 0 }}
+        configureScene = {(route) => {
+          return Navigator.SceneConfigs.FloatFromBottomAndroid;
+        }}
+        renderScene={(route, navigator) =>
+        	<View>
+        		<Toolbar onIcon ={this.onIcon.bind(this)}/>
+        		<MyScene
+	            title={route.title}        
+	            onForward={ () => {    
+	              const nextIndex = route.index + 1;
+	              navigator.push({
+	                title: 'Scene ' + nextIndex,
+	                index: nextIndex,
+	              });
+	            }}
+	            onBack={() => {
+	              if (route.index > 0) {
+	                navigator.pop();
+	              }
+	            }}
+	          />
+        	</View>
+        }
+      />
+    )
+  }
+}
+
+class MyScene extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    onForward: PropTypes.func.isRequired,
+    onBack: PropTypes.func.isRequired,
+  }
+  render() {
+    return (
+      <View>
+        <Text>Current Scene: { this.props.title }</Text>
+        <TouchableHighlight onPress={this.props.onForward}>
+          <Text>Tap me to load the next scene</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this.props.onBack}>
+          <Text>Tap me to go back</Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+}
